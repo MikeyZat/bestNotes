3cb249ad3fd50c90c5cbdbcaf6e04e7e7e3a6e8e
MikeyZat <mikeyzat@gmail.com>
2018-12-01 23:15:02 +0100

    Updates

---
+++ precache-manifest.bee9a1d61c3dfaabd12f6730d22d0c31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d06077c87e8573e1cb74",
+    "url": "/bestNotes/static/css/main.adefddba.chunk.css"
+  },
+  {
+    "revision": "d06077c87e8573e1cb74",
+    "url": "/bestNotes/static/js/main.d06077c8.chunk.js"
+  },
+  {
+    "revision": "c40ebc2deb63521971cc",
+    "url": "/bestNotes/static/js/1.c40ebc2d.chunk.js"
+  },
+  {
+    "revision": "bd42ae98ce27e36a2243",
+    "url": "/bestNotes/static/js/runtime~main.bd42ae98.js"
+  },
+  {
+    "revision": "cbd50882df82be5fce046e52b302e316",
+    "url": "/bestNotes/static/media/IndieFlower.cbd50882.ttf"
+  },
+  {
+    "revision": "1f4014ae83a79899b9829781ee43dbac",
+    "url": "/bestNotes/index.html"
+  }
+];
